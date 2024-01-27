9cebbadec70b28873b10ecadf49b101d5b13bd7f
Muhammed Onur Kaya <100594545+onurkya7@users.noreply.github.com>
2024-01-27 23:34:32 +0300

    Home page addition

--- ConciFest/ConciFest/Default.aspx.cs
+++ ConciFest/ConciFest/Default.aspx.cs
@@ -1,17 +1,85 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
+using System.Net;
 using System.Web;
+using System.Web.Configuration;
+using System.Web.Optimization;
 using System.Web.UI;
+using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 
-namespace WebApplication1
+namespace ConciFes
 {
-    public partial class _Default : Page
+    public partial class Default : System.Web.UI.Page
     {
+        string conf_baglanti = WebConfigurationManager.ConnectionStrings["ConciFestConnectionString"].ConnectionString;
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (Session["Kullanici"] == null)
+            {
+                // Kullanıcı oturumu yoksa default sayfasında kal
+                var masterPage = this.Master as GuestPage;
+                if (masterPage != null)
+                {
+                    var sectionContainer = masterPage.FindControl("sectionContainer") as HtmlGenericControl;
+                    if (sectionContainer != null)
+                    {
+                        sectionContainer.Attributes["class"] = "section section-defaultpage";
+                    }
+                }
+            }
+            else
+            {
+                // Kullanıcı oturumu varsa üye sayfasına yönlendir
+                Response.Redirect("~/Uye.aspx");
+            }
         }
+
+        protected void buttonSend_Click(object sender, EventArgs e)
+        {
+            // Formdaki bilgileri al
+            string name = txtName.Text.ToString();
+            string email = txtEmail.Text.ToString();
+            string company = txtCompany.Text.ToString();
+            string message = TextMsg.Text.ToString();
+
+            try
+            {
+                // Outlook SMTP ayarları
+                SmtpClient client = new SmtpClient("smtp-mail.outlook.com", 587);
+                client.EnableSsl = true;
+                client.UseDefaultCredentials = false;
+                client.Credentials = new NetworkCredential("ahmet3434try@outlook.com", "34343434!");
+
+                // E-posta oluştur
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress("ahmet3434try@outlook.com");
+                mail.To.Add("onur2323den@outlook.com");
+                mail.Subject = "ConciFest İletişim";
+                mail.Body = $"İsim: {name}\nEmail: {email}\nKurum İsmi: {company}\nMesaj: {message}";
+
+                // E-postayı gönder
+                client.Send(mail);
+
+                // İletişim formunu temizle
+                txtName.Text = "";
+                txtEmail.Text = "";
+                txtCompany.Text = "";
+                TextMsg.Text = "";
+
+                // Başarılı bir şekilde gönderildiğini göster
+                lblMesaj.Text = "Mesajınız başarıyla iletildi..";
+                lblMesaj.Visible = true;
+            }
+            catch (Exception ex)
+            {
+                // Hata durumunda hatayı göster
+                lblMesaj.Text = "<script>alert('Mesaj gönderilirken bir hata oluştu. Lütfen daha sonra tekrar deneyin.');</script>";
+                lblMesaj.Visible = true;
+            }
+        }
+
     }
 }
