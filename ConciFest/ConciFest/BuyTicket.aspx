26771ce244f190b94c34f0a882d661de3a992b5f
Muhammed Onur Kaya <100594545+onurkya7@users.noreply.github.com>
2024-02-01 22:12:36 +0300

    Ticket purchase page added

---
+++ ConciFest/ConciFest/BuyTicket.aspx
@@ -0,0 +1,60 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MemberPage.Master" AutoEventWireup="true" CodeBehind="BuyTicket.aspx.cs" Inherits="ConciFes.BuyTicket1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
+    <div id="header" runat="server">
+            <header class="site-header">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12 col-12 d-flex flex-wrap">
+                            <p class="d-flex me-4 mb-0">
+                                <i class="bi-person custom-icon me-2"></i>
+                                <strong class="text-dark">Konser ve Festival Tanıtımlarına Hoşgeldiniz</strong>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </header>
+    </div>
+    <div id="section" runat="server" class="section section-buyticketpage"> 
+        <section class="ticket-section section-padding">
+                <div class="section-overlay"></div>
+
+                <div class="container">
+                    <div class="row">
+
+                        <div class="col-lg-6 col-10 mx-auto">
+                            <div class="custom-form ticket-form mb-5 mb-lg-0">
+                                <h2 class="text-center mb-4">Bilet alma</h2>
+
+                                <div class="ticket-form-body">
+                                    <div class="row">
+                                        <div class="col-lg-6 col-md-6 col-12">
+                                            <asp:TextBox ID="TextUserName" CssClass="form-control" runat="server" style="border-color: orange;"></asp:TextBox>
+                                        </div>
+
+                                        <div class="col-lg-6 col-md-6 col-12">
+                                            <asp:TextBox ID="TextEmail" CssClass="form-control" runat="server" style="border-color: orange;"></asp:TextBox>
+                                        </div>
+
+                                    </div>
+
+                                    <asp:TextBox ID="TextTelefon" placeholder= "Telefon Numarası" CssClass="form-control" runat="server"></asp:TextBox>
+                                    <h6>Etkinlik tipini seç</h6>
+
+                                    <asp:DropDownList ID="DropDownListOptions" runat="server" style="margin-bottom: 20px;" CssClass="form-control">
+                                    </asp:DropDownList>
+
+
+                                    <textarea name="ticket-form-message" rows="3" class="form-control"
+                                        id="ticket-form-message" placeholder="Ek Talep"></textarea>
+
+                                    <div class="col-lg-4 col-md-10 col-8 mx-auto">
+                                         <asp:Button ID="Button1" CssClass="form-control" runat="server" Text="Bilet Al" style="background-color: #ee5007; color: white; margin-top: 10px;" OnClick="Button1_Click" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                 </div>
+        </section>
+        </div>
+</asp:Content>
---
+++ ConciFest/ConciFest/BuyTicket.aspx.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+namespace ConciFes
+{
+    public partial class BuyTicket1 : System.Web.UI.Page
+    {
+        string conf_baglanti = WebConfigurationManager.ConnectionStrings["ConciFestConnectionString"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["Kullanici"] != null)
+            {
+                // Kullanıcı oturumu yoksa default sayfasında kal
+                var masterPage = this.Master as GuestPage;
+                if (masterPage != null)
+                {
+                    var sectionContainer = masterPage.FindControl("sectionContainer") as HtmlGenericControl;
+                    if (sectionContainer != null)
+                    {
+                        sectionContainer.Attributes["class"] = "section section-buyticketpage";
+                    }
+                }
+
+                string kullaniciAdi = Session["Kullanici"].ToString();
+
+                SqlConnection baglanti = new SqlConnection(conf_baglanti);
+                baglanti.Open();
+
+                SqlCommand kullaniciBilgiKomut = new SqlCommand("SELECT UserName, Mail FROM tblKullanici WHERE UserName = @UserName", baglanti);
+                kullaniciBilgiKomut.Parameters.AddWithValue("@UserName", kullaniciAdi);
+
+                using (SqlDataReader kullaniciBilgiReader = kullaniciBilgiKomut.ExecuteReader())
+                {
+                    if (kullaniciBilgiReader.Read())
+                    {
+                        string email = kullaniciBilgiReader["Mail"].ToString();
+
+                        TextUserName.Text = kullaniciAdi;
+                        TextEmail.Text = email;
+                    }
+                    kullaniciBilgiReader.Close();
+                }
+
+                SqlCommand planBilgiKomut = new SqlCommand("SELECT Etkinlik, CONCAT(Isim, ' ', Soyisim) AS AdSoyad, Tarih_gun, Tarih_ay FROM tblPlan", baglanti);
+
+                using (SqlDataReader planBilgiReader = planBilgiKomut.ExecuteReader())
+                {
+                    while (planBilgiReader.Read())
+                    {
+                        string etkinlik = planBilgiReader["Etkinlik"].ToString();
+                        string adSoyad = planBilgiReader["AdSoyad"].ToString();
+                        string tarihGun = planBilgiReader["Tarih_gun"].ToString();
+                        string tarihAy = planBilgiReader["Tarih_ay"].ToString();
+
+                        ListItem listItem = new ListItem(adSoyad + " - " + etkinlik + " - " + tarihGun + " " + tarihAy, etkinlik);
+                        DropDownListOptions.Items.Add(listItem);
+                    }
+                    planBilgiReader.Close();
+                }
+                baglanti.Close();
+
+            }
+
+
+            else
+            {
+                // Kullanıcı oturumu varsa üye sayfasına yönlendir
+                Response.Redirect("~/LoginPage.aspx");
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            Response.Redirect("~/Payment.aspx");
+        }
+    }
+}
