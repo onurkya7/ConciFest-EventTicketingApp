8c8ce32b0b3fd83990a112908bcd4729e48c5d21
Muhammed Onur Kaya <100594545+onurkya7@users.noreply.github.com>
2024-02-02 14:35:47 +0300

    Package purchase page added

---
+++ ConciFest/ConciFest/BuyPacket.aspx.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+namespace ConciFes
+{
+    public partial class BuyPacket : System.Web.UI.Page
+    {
+        string conf_baglanti = WebConfigurationManager.ConnectionStrings["ConciFestConnectionString"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["Kullanici"] != null)
+            {
+                // Kullanıcı oturumu yoksa default sayfasında kal
+                var masterPage = this.Master as GuestPage;
+                if (masterPage != null)
+                {
+                    var sectionContainer = masterPage.FindControl("sectionContainer") as HtmlGenericControl;
+                    if (sectionContainer != null)
+                    {
+                        sectionContainer.Attributes["class"] = "section section-buypacketpage";
+                    }
+                }
+
+                string kullaniciAdi = Session["Kullanici"].ToString();
+
+                SqlConnection baglanti = new SqlConnection(conf_baglanti);
+                baglanti.Open();
+
+                SqlCommand kullaniciBilgiKomut = new SqlCommand("SELECT UserName, Mail FROM tblKullanici WHERE UserName = @UserName", baglanti);
+                kullaniciBilgiKomut.Parameters.AddWithValue("@UserName", kullaniciAdi);
+
+                using (SqlDataReader kullaniciBilgiReader = kullaniciBilgiKomut.ExecuteReader())
+                {
+                    if (kullaniciBilgiReader.Read())
+                    {
+                        string email = kullaniciBilgiReader["Mail"].ToString();
+
+                        TextUserName.Text = kullaniciAdi;
+                        TextEmail.Text = email;
+                    }
+                    kullaniciBilgiReader.Close();
+                }
+
+                SqlCommand planBilgiKomut = new SqlCommand("SELECT Isim, Fiyat FROM tblPaket", baglanti);
+
+                using (SqlDataReader planBilgiReader = planBilgiKomut.ExecuteReader())
+                {
+                    while (planBilgiReader.Read())
+                    {
+                        string isim = planBilgiReader["Isim"].ToString();
+                        string fiyat = planBilgiReader["Fiyat"].ToString();
+
+                        ListItem listItem = new ListItem(isim + " - " + fiyat + " TL");
+                        DropDownListOptions.Items.Add(listItem);
+                    }
+                    planBilgiReader.Close();
+                }
+
+                baglanti.Close();
+            }
+
+            else
+            {
+                // Kullanıcı oturumu varsa üye sayfasına yönlendir
+                Response.Redirect("~/LoginPage.aspx");
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            Response.Redirect("~/Payment.aspx");
+        }
+
+
+    }
+}
---
+++ ConciFest/ConciFest/BuyPacket.aspx.designer.cs
@@ -0,0 +1,80 @@
+﻿//------------------------------------------------------------------------------
+// <otomatik olarak oluşturulmuş>
+//     Bu kod bir araç tarafından oluşturuldu.
+//
+//     Bu dosyada yapılacak değişiklikler hatalı davranışa neden olabilir ve
+//     kod tekrar üretildi. 
+// </otomatik olarak oluşturulmuş>
+//------------------------------------------------------------------------------
+
+namespace ConciFes
+{
+
+
+    public partial class BuyPacket
+    {
+
+        /// <summary>
+        /// header denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl header;
+
+        /// <summary>
+        /// section denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl section;
+
+        /// <summary>
+        /// TextUserName denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextUserName;
+
+        /// <summary>
+        /// TextEmail denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextEmail;
+
+        /// <summary>
+        /// TextTelefon denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextTelefon;
+
+        /// <summary>
+        /// DropDownListOptions denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownListOptions;
+
+        /// <summary>
+        /// Button1 denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik olarak oluşturulan alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+    }
+}
